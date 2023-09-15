3a6bf248e2a6e78081f81e4e93c562113b3cc299
k.stopczynska <k.stopczynska@gmail.com>
2023-09-15 12:10:31 +0200

    environmental variables for api key

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
+
 def main():
     print("Hello world")
+    print(API_KEY)
+
 
 if __name__ == "__main__":
     main()
