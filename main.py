4d12cdf145b43f7a77b8f4d74357fc5a8a53a8f4
k.stopczynska <k.stopczynska@gmail.com>
2023-09-15 13:00:00 +0200

    getting searched term from the user

--- main.py
+++ main.py
@@ -7,21 +7,28 @@ load_dotenv()
 API_KEY = os.getenv('API_KEY')
 API_HOST = "shazam.p.rapidapi.com"
 BASE_URL = "https://shazam.p.rapidapi.com/auto-complete"
-querystring = {'term': 'beautiful', 'locale': 'en-US'}
+# querystring = {'term': 'beautiful', 'locale': 'en-US'}
 headers = {
     "X-RapidAPI-Key": API_KEY,
     "X-Rapid-Api-Host": API_HOST,
 }
 printer = PrettyPrinter()
 
+def get_searched_term():
+    searched_term = input("What song or artist are you looking for? ")
+    return searched_term
+
 def get_response(url, headers, params):
     response = requests.get(url, headers=headers, params=params)
     return response
 
+
 def main():
+    search = get_searched_term()
+    print(search)
+    querystring = {'term': search, 'locale': 'en-US'}
     data = get_response(BASE_URL, headers, querystring)
     printer.pprint(data.json())
-
 
 
 if __name__ == "__main__":
