175b5bb50003c78b248d024b5463f127d4803bcf
k.stopczynska <k.stopczynska@gmail.com>
2023-09-15 12:44:36 +0200

    getting pretty printed response

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
 import os
+import requests
 from dotenv import load_dotenv
+from pprint import PrettyPrinter
 load_dotenv()
 
 API_KEY = os.getenv('API_KEY')
+API_HOST = "shazam.p.rapidapi.com"
+BASE_URL = "https://shazam.p.rapidapi.com/auto-complete"
+querystring = {'term': 'beautiful', 'locale': 'en-US'}
+headers = {
+    "X-RapidAPI-Key": API_KEY,
+    "X-Rapid-Api-Host": API_HOST,
+}
+printer = PrettyPrinter()
+
+def get_response(url, headers, params):
+    response = requests.get(url, headers=headers, params=params)
+    return response
 
 def main():
-    print("Hello world")
-    print(API_KEY)
+    data = get_response(BASE_URL, headers, querystring)
+    printer.pprint(data.json())
+
 
 
 if __name__ == "__main__":
